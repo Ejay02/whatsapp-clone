8755f14e5b2dc06f6cec227f34c6eb790fd95ddd
Ejay02 <e.jae02@gmail.com>
2022-04-05 22:02:42 +0100

    whasapp clone project setup

---
+++ src/components/chats/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="bg-wa-dark text-white">
+      <h1>Whatsapp</h1>
+      <div>
+        <span class="material-icons">search</span>
+        <span class="material-icons">chat</span>
+        <span class="material-icons">more_vert</span>
+      </div>
+      <div>CALLS CHATS CONTACTS</div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderTab",
+};
+</script>
+
+<style></style>
---
+++ src/views/Chats.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+    <Header/>
+    <section class="m-2 flex">
+      <div>
+        <img src="../assets/8.jpg"
+        class="h-12 w-12 rounded-full"/>
+      </div>
+      <article >
+        <div class="flex">
+          <h2>Name</h2>
+          <p>Time</p>
+        </div>
+        <div class="flex">
+          <p>message</p>
+          <p>count</p>
+        </div>
+      </article>
+    </section>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/chats/Header.vue';
+export default {
+  name: "ChatsPage",
+  // eslint-disable-next-line vue/no-unused-components
+  components: { Header },
+
+  setup(){
+    return {
+      Header
+    };
+  },
+  
+};
+</script>
+
+<style></style>
